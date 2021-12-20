5c36d86223396ec64af89ca351e9f881b4362b48
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-20 19:07:30 +0200

    Search for availability by room
    ask the database how many rows in the room restrictions table do you have where my dates overlap in any way, shape or form your dates and if I get zero that I know the room's available for those dates and if I get one or more that I know there's a reservation that blocks that date range.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,12 @@
 package repository
 
-import "github.com/ahmedkhaeld/bookings/internal/models"
+import (
+	"github.com/ahmedkhaeld/bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
