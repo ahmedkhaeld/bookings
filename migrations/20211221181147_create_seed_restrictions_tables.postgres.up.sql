d1357bba5a5e922728d250ea269affac6c6e372f
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-22 13:54:16 +0200

    Add a migration for seeding restrictions, and rooms

---
+++ migrations/20211221181147_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2021-11-07 00:00:00.000','2021-11-08 00:00:00.000'),
+('Owner block','2022-01-01 00:00:00.000','2022-01-05 00:00:00.000');
