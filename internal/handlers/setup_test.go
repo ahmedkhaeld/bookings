a6d95dd3c3ebb5523e698e5bed384116b299574f
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-24 00:24:45 +0200

    Update handler test after adding listenForMail()

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -47,6 +47,12 @@ func TestMain(m *testing.M) {
 
 	app.Session = session
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(mailChan)
+
+	listenForMail()
+
 	// 4. create the template cache
 	templateCache, err := CreateTestTemplateCache()
 	if err != nil {
@@ -65,6 +71,14 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 
 	os.Exit(m.Run())
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 // getRoutes return routes http handlers
